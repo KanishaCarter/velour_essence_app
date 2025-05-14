3b5572890a57251794571de61c5a164ded9703f7
KanishaCarter <kanishacarter90@outlook.com>
2025-05-13 20:05:02 -0500

    created components folder and added mock data to data.js file

---
+++ velour_essence_app/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import ScentList from './components/data.js'
+import './App.css'
+
+function App() {
+  return (
+    <body>
+      <h1>Available Scents</h1>
+    <div id = "scentsContainer">
+      <ul>
+        {ScentList.map((scent) => (
+          <li key={scent.id}>
+            <h3>{scent.name}</h3>
+            <p>Designer: {scent.designer}</p>
+            <p>Size: {scent.size} oz</p>
+            <p>Price: ${scent.price}</p>
+            <p>Scent Family: {scent.family}</p>
+            <p>Scent Type: {scent.type}</p>
+            <p>Scent Notes: {scent.notes}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+    </body>
+  );
+}
+
+
+
+export default App
---
+++ velour_essence_app/src/components/data.js
@@ -0,0 +1,48 @@
+class perfume {
+    constructor (id, name, designer, size, price, family, type, notes){
+        this.id = id;
+        this.img = img;
+        this.name = name;
+        this.designer = designer;
+        this.size = size;
+        this.price = price;
+        this.family = family;
+        this.type = type;
+        this.notes = notes;
+    }
+}
+
+const ScentList = [
+    {
+        id: 1,
+        name: "Lost Cherry Eau de Parfum Fragrance",
+        designer: "Tom Ford",
+        size: 3.4,
+        price: 615.00,
+        family: "Warm & Spicy",
+        type: "Warm & Sweet Gourmands",
+        notes: "Black Cherry, Tonka Bean, Almond",
+    },
+    {
+        id: 2,
+        name: "Donna Born In Roma Eau de Parfum",
+        designer: "Valentino",
+        size: 1.7,
+        price: 140.00,
+        family: "Floral",
+        type: "Warm Florals",
+        notes: "Blackcurrant, Jasmine Grandiflorum, Bourbon Vanilla",
+    },
+    {
+        id: 3,
+        name: "Mon Paris Eau de Parfum",
+        designer: "Yves Saint Laurent",
+        size: 1.6,
+        price: 120.00,
+        family: "Floral",
+        type: "Fruity Floral",
+        notes: "Datura Flower, Patchouli, Red Berries",
+    }
+]
+ 
+export default ScentList
