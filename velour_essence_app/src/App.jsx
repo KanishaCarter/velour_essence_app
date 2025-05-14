b78236694f3a48de15202122f16babdfd851961a
KanishaCarter <kanishacarter90@outlook.com>
2025-05-13 20:43:07 -0500

    Created Scents.jsx file to fetch all available scents and allow the user to search for a specific scent by the scent name or designer name.

--- velour_essence_app/src/App.jsx
+++ velour_essence_app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import ScentList from './components/data.js'
+import 
 import './App.css'
 
 function App() {
---
+++ velour_essence_app/src/components/Scents.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function AvailableScents() {
+  const [scents, setScents] = useState([]);
+  const [searchScent, setSearchScent] = useState('');
+
+  useEffect(() => {
+    async function fetchScents() {
+      try {
+        const response = await fetch('http://localhost:5173/api/scents');
+        const data = await response.json();
+        setScents(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchScents();
+  }, []);
+
+  const filteredScents = scents.filter(
+    scents => scent.name.toLowerCase().includes(searchTerm.toLowerCase()) || 
+            scent.designer.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Available Scents</h1>
+      <input
+        type="text"
+        placeholder="Search by scent name or designer name"
+        value={searchScent}
+        onChange={(e) => setSearchScent(e.target.value)}
+      />
+      <ul>
+        {filteredScents.map(scent => (
+          <li key={scent.id}>
+            <Link to={`/scents/${scent.id}`}>{scent.name} by {scent.designer}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default AvailableScents;
