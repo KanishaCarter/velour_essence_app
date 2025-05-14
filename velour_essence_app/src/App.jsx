89a0906de62805365ad4b838f227fa0450fb1d7f
KanishaCarter <kanishacarter90@outlook.com>
2025-05-13 21:00:22 -0500

    changed Home.jsx to Login.jsx because the user will use this screen to login. If they do not have an account, they will need to sign up. Also created an empty app function to be updated at a later time.

--- velour_essence_app/src/App.jsx
+++ velour_essence_app/src/App.jsx
@@ -1,29 +1,11 @@
 import { useState } from 'react'
 import ScentList from './components/data.js'
 import AvailableScents from './components/Scents.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
-  return (
-    <body>
-      <h1>Available Scents</h1>
-    <div id = "scentsContainer">
-      <ul>
-        {ScentList.map((scent) => (
-          <li key={scent.id}>
-            <h3>{scent.name}</h3>
-            <p>Designer: {scent.designer}</p>
-            <p>Size: {scent.size} oz</p>
-            <p>Price: ${scent.price}</p>
-            <p>Scent Family: {scent.family}</p>
-            <p>Scent Type: {scent.type}</p>
-            <p>Scent Notes: {scent.notes}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-    </body>
-  );
+ 
 }
 
 
