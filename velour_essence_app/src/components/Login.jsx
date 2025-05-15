6962346c43ef92010e6920f9f2bc7292828d07b9
KanishaCarter <kanishacarter90@outlook.com>
2025-05-15 16:20:57 -0500

    edited Login.jsx to add event handler function handleLogin to prevent app from reloading after user enters information into the form to submit.

--- velour_essence_app/src/components/Login.jsx
+++ velour_essence_app/src/components/Login.jsx
@@ -4,12 +4,15 @@ import { useState } from 'react';
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  
 
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
+  
   return (
     <form onSubmit={handleLogin}>
       <h1>Login</h1>
-      <input type="username" value={username} onChange={event => setUsername(event.target.value)} placeholder="Username" required />
+      <input type="text" value={username} onChange={event => setUsername(event.target.value)} placeholder="Username" required />
       <input type="password" value={password} onChange={event => setPassword(event.target.value)} placeholder="Password" required />
       <button type="submit">Login</button>
     </form>
