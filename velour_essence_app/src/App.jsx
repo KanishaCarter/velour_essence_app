58d80dc3e386aeb4d002039c7a05f2f15b1b5f7c
KanishaCarter <kanishacarter90@outlook.com>
2025-05-21 17:16:03 -0500

    created additional front end files for logging in, signing up, navigating through the app, viewing a single scent, and viewing all available scents. Created routes in the app.jsx file.

--- velour_essence_app/src/App.jsx
+++ velour_essence_app/src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
-import AvailableScents from './components/Scents.jsx'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import AvailableScents from './components/Scents';
+import ScentCard from './components/ScentCard';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout'; 
+
 
 function App() {
- 
+  return (
+    <BrowserRouter>
+    <Navigation/>
+    <Routes>
+        <Route path="/" element = {<ScentCard/>}/>
+        <Route path="/login" element = {<Login/>}/>
+        <Route path="/signup" element = {<Signup/>}/>
+        <Route path="/cart" element = {<Cart/>}/>
+        <Route path="/checkout" element = {<ScentCard/>}/>
+        <Route path="/" element = {<ScentCard/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
+export default App;
 
-
-export default App
