98611f952b696fa5223cf2bbc1507b3d1688294a
KanishaCarter <kanishacarter90@outlook.com>
2025-05-21 15:33:40 -0500

    adjusted the main.jsx file to wrap the App component with BrowserRouter tag so the app has access to to React Router API

--- velour_essence_app/src/App.jsx
+++ velour_essence_app/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import ScentList from './components/data.js'
 import AvailableScents from './components/Scents.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
