c364175a893591d7cbfe3ccc2ab4489e381838c6
KanishaCarter <kanishacarter90@outlook.com>
2025-05-21 16:18:55 -0500

    added static and server routes to index.js.

--- server/index.js
+++ server/index.js
@@ -2,10 +2,8 @@ const express = require("express");
 const server = express();
 const pg = require("pg");
 const path = require("path");
-
-const client = new pg.Client(
-    process.env.DATABASE_URL || "postgres://localhost/velour_essence_app"
-);
+const dotenv = require("dotenv").config();
+const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost/velour_essence_app");
 
 const init = async () => {
     await client.connect();
@@ -27,5 +25,34 @@ const init = async () => {
     INSERT INTO scents (name, designer, size, price, family, type, notes) VALUES
     ("Lost Cherry Eau de Parfum Fragrance", "Tom Ford", 3.4, 615.00, "Warm & Spicy", "Warm & Sweet Gourmands", "Black Cherry, Tonka Bean, Almond"),
     ("Donna Born In Roma Eau de Parfum", "Valentino", 1.7, 140.00, "Floral", "Warm Florals", "Blackcurrant, Jasmine Grandiflorum, Bourbon Vanilla"),
-    ("Mon Paris Eau de Parfum", "Yves Saint Laurent", 1.6, 120.00, "Floral", "Fruity Floral", "Datura Flower, Patchouli, Red Berries")`
-}
+    ("Mon Paris Eau de Parfum", "Yves Saint Laurent", 1.6, 120.00, "Floral", "Fruity Floral", "Datura Flower, Patchouli, Red Berries");`;
+
+    await client.query(SQL);
+    console.log("data updated");
+
+    const port = process.env.PORT || 3000;
+    server.listen(port, () => console.log (`listening on port ${port}`));
+};
+
+init();
+
+// static routes
+server.use(express.static(path.join(__dirname, "../client/dist")));
+
+// server routes
+server.get("/", (req, res) =>
+res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+);
+
+// returns array of scent objects
+server.get("/api/scents", async (req, res, next) => {
+    try {
+        const SQL = `
+        SELECT * from scents;
+        `;
+        const response = await client.query(SQL);
+        res.send(response.rows);
+    } catch (ex) {
+        next(ex);
+    }
+});
